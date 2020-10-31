3a92279df5cde570cd18d82304ad9f08e579a243
prakhar0010 <prakhar00100@gmail.com>
2020-10-31 01:52:42 -0700

    password4

--- day14ass.sh
+++ day14ass.sh
@@ -1,12 +1,13 @@
 #!bin/bash -x
 
-#for password with numeric number
+#for password with special character
 
 
-read -p "enter a pasword " A
+read -p "enter a pasword " B
+
 p7='^[A-Z]{1}[+a-zA-Z0-9]{8,}$'
 
-if [[ $A =~ $p7 ]]
+if [[ $B =~ $p7 ]]
 then
     echo "yes"
 else
